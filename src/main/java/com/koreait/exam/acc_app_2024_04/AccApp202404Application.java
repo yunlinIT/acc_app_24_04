41899e14a3ebf8286d4190d8a2f7417c073095ea
yunlinIT <yunlinit@gmail.com>
2024-04-25 16:55:43 +0900

    프로젝트 세팅

---
+++ src/main/java/com/koreait/exam/acc_app_2024_04/AccApp202404Application.java
@@ -0,0 +1,13 @@
+package com.koreait.exam.acc_app_2024_04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccApp202404Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccApp202404Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/koreait/exam/acc_app_2024_04/AccApp202404ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koreait.exam.acc_app_2024_04;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccApp202404ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
