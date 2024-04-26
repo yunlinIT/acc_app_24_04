5f160d57b732ca3de6ded7666e6b8c7a03b8365b
yunlinIT <yunlinit@gmail.com>
2024-04-26 16:24:54 +0900

    스프링 시큐리티, 로그인, 로그아웃, 회원가입 3

---
+++ src/main/java/com/koreait/exam/acc_app_2024_04/app/security/service/CustomUserDetailsService.java
@@ -0,0 +1,31 @@
+package com.koreait.exam.acc_app_2024_04.app.security.service;
+
+import com.koreait.exam.acc_app_2024_04.app.member.entity.Member;
+import com.koreait.exam.acc_app_2024_04.app.member.repository.MemberRepository;
+import com.koreait.exam.acc_app_2024_04.app.security.dto.MemberContext;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+    private final MemberRepository memberRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Member member = memberRepository.findByUsername(username).get();
+
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority("MEMBER"));
+
+        return new MemberContext(member, authorities);
+    }
+}
