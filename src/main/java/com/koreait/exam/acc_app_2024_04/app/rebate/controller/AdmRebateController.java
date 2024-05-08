95c6a289c9f0e8f9da8bece3a15e1ebea1e34dd5
yunlinIT <yunlinit@gmail.com>
2024-05-08 15:17:57 +0900

    정산데이터 생성 페이지 추가

---
+++ src/main/java/com/koreait/exam/acc_app_2024_04/app/rebate/controller/AdmRebateController.java
@@ -0,0 +1,16 @@
+package com.koreait.exam.acc_app_2024_04.app.rebate.controller;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/adm/rebate")
+public class AdmRebateController {
+    @GetMapping("/makeData")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public String showMakeData(){
+        return "adm/rebate/makeData";
+    }
+}
