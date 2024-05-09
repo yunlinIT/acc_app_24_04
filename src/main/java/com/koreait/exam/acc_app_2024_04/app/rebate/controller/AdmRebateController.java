aa09195f133c233e1e0452b4e782c0c5bbc9afa8
yunlinIT <yunlinit@gmail.com>
2024-05-09 14:28:08 +0900

    RebateOrderItem 목록 구현

--- src/main/java/com/koreait/exam/acc_app_2024_04/app/rebate/controller/AdmRebateController.java
+++ src/main/java/com/koreait/exam/acc_app_2024_04/app/rebate/controller/AdmRebateController.java
@@ -1,16 +1,19 @@
 package com.koreait.exam.acc_app_2024_04.app.rebate.controller;
 
+import com.koreait.exam.acc_app_2024_04.app.rebate.entity.RebateOrderItem;
 import com.koreait.exam.acc_app_2024_04.app.rebate.service.RebateService;
 import com.koreait.exam.acc_app_2024_04.util.Ut;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Controller
 @RequestMapping("/adm/rebate")
@@ -36,12 +39,19 @@ public class AdmRebateController {
 
     @GetMapping("/rebateOrderItemList")
     @PreAuthorize("hasAuthority('ADMIN')")
-    public String showRebateOrderItemList(String yearMonth) {
+    public String showRebateOrderItemList(String yearMonth, Model model) {
         if (yearMonth == null) {
-            yearMonth = "2022-10";
+            yearMonth = "2024-05";
         }
+
+        List<RebateOrderItem> items = rebateService.findRebateOrderItemsByPayDateIn(yearMonth);
+
+        model.addAttribute("items", items);
 
         return "adm/rebate/rebateOrderItemList";
     }
+
+
+
 
 }
--- src/main/java/com/koreait/exam/acc_app_2024_04/app/rebate/repository/RebateOrderItemRepository.java
+++ src/main/java/com/koreait/exam/acc_app_2024_04/app/rebate/repository/RebateOrderItemRepository.java
@@ -3,8 +3,12 @@ package com.koreait.exam.acc_app_2024_04.app.rebate.repository;
 import com.koreait.exam.acc_app_2024_04.app.rebate.entity.RebateOrderItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 public interface RebateOrderItemRepository extends JpaRepository<RebateOrderItem, Long> {
     Optional<RebateOrderItem> findByOrderItemId(long orderItemId);
+
+    List<RebateOrderItem> findAllByPayDateBetweenOrderByIdAsc(LocalDateTime fromDate, LocalDateTime toDate);
 }
