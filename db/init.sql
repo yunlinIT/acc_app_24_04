24c93a91bcea7342b628f65cf7679bbc6d16630f
yunlinIT <yunlinit@gmail.com>
2024-05-09 13:48:06 +0900

    주문품목당 정산금액 산정 SQL

--- db/init.sql
+++ db/init.sql
@@ -7,3 +7,12 @@ USE acc_app_2024_04__test;
 DROP DATABASE IF EXISTS acc_app_2024_04__dev;
 CREATE DATABASE acc_app_2024_04__dev;
 USE acc_app_2024_04__dev;
+
+
+-- SELECT order_item_id, pay_price, refund_price, wholesale_price, pg_fee,
+--        CASE
+--            WHEN pay_price = refund_price
+--                THEN 0
+--            ELSE wholesale_price - pg_fee
+--            END AS rebate_price
+-- FROM `rebate_order_item`;
