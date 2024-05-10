844a9cb7c99bbc4a6445ab87bacfc5215c838649
yunlinIT <yunlinit@gmail.com>
2024-05-10 13:20:30 +0900

    장바구니에 담긴 품목들 주문하기

--- src/main/java/com/koreait/exam/acc_app_2024_04/app/order/controller/OrderController.java
+++ src/main/java/com/koreait/exam/acc_app_2024_04/app/order/controller/OrderController.java
@@ -153,4 +153,14 @@ public class OrderController {
         return "order/fail";
     }
 
+    @PostMapping("/makeOrder")
+    @PreAuthorize("isAuthenticated()")
+    public String makeOrder(@AuthenticationPrincipal MemberContext memberContext) {
+        Member member = memberContext.getMember();
+        Order order = orderService.createFromCart(member);
+        String redirect = "redirect:/order/%d".formatted(order.getId()) + "?msg=" + Ut.url.encode("%d번 주문이 생성되었습니다.".formatted(order.getId()));
+
+        return redirect;
+    }
+
 }
